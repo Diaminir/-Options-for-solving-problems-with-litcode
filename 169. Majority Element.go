2befdb0c89bf381b841122ce881919f5e8ac59d1
Diaminir <dima.drozdov.20124@gmail.com>
2025-09-18 16:37:41 +0300

    Create  169. Majority Element.go

---
+++ 169. Majority Element.go
@@ -0,0 +1,22 @@
+func majorityElement(nums []int) int {
+mapNums := make(map[int]int)
+	for _, v := range nums {
+		mapNums[v]++
+	}
+	var max int
+	fmt.Println(mapNums)
+	for k, v := range mapNums {
+		if v > mapNums[max] {
+			max = k
+		}
+	}
+	return max
+}
+
+func main() {
+	s1 := []int{3, 2, 3}
+	fmt.Println(majorityElement(s1))
+
+	s2 := []int{2, 2, 1, 1, 1, 2, 2}
+	fmt.Println(majorityElement(s2))
+}
